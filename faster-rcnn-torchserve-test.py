c1dd94779349af70f38c0734afbcdb58d075a8f4
octopusbrolau <liugegame@163.com>
2022-07-07 21:14:45 +0800

    ddd

---
+++ faster-rcnn-torchserve-test.py
@@ -0,0 +1,93 @@
+
+
+
+from __future__ import print_function
+
+import io
+import os
+import time
+import base64
+import json
+import time
+
+import requests
+import pickle
+import onepanel.core.api
+from onepanel.core.api.rest import ApiException
+import onepanel.core.auth
+from pprint import pprint
+
+
+# ## Get Onepanel Access Token for network requests
+
+# In[2]:
+
+
+# If inside of Onepanel, get mounted service account token to use as API Key
+access_token = onepanel.core.auth.get_access_token()
+
+print('---ONEPANEL_API_URL----', os.getenv('ONEPANEL_API_URL'))
+# Configure API key authorization: Bearer
+configuration = onepanel.core.api.Configuration(
+    host=os.getenv('ONEPANEL_API_URL'),
+    api_key={
+        'authorization': access_token
+    }
+)
+configuration.api_key_prefix['authorization'] = 'Bearer'
+
+
+# In[5]:
+
+
+namespace = 'mp'
+model_name = 'faster-rcnn-torchserve'
+
+
+# In[6]:
+
+
+# Get status, endpoint
+with onepanel.core.api.ApiClient(configuration) as api_client:
+    api_instance = onepanel.core.api.InferenceServiceApi(api_client)
+
+    try:
+        ready = False
+        while not ready:
+            api_response = api_instance.get_inference_service(namespace, model_name)
+            ready = api_response.ready
+            endpoint = api_response.predict_url
+            print('---api_response.predict_url---', endpoint)
+            time.sleep(1)
+    except ApiException as e:
+        print("Exception when calling InferenceServiceApi->get_inference_service_status: %s\n" % e)
+
+
+# In[8]:
+
+with open('./img.pkl','rb') as f:
+    img_data = pickle.load(f)
+
+data = {
+    'instances': img_data
+}
+
+
+headers = {
+    'onepanel-access-token': access_token
+}
+
+print('headers', headers)
+print('endpoint', endpoint)
+r = requests.post(endpoint, headers=headers, json=data)
+
+result = r.json()
+
+print(result)
+
+
+# In[ ]:
+
+
+
+
