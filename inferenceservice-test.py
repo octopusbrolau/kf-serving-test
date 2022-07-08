04156f23e609057b9978aa71dca6fa0e3411e8c2
octopusbrolau <liugegame@163.com>
2022-07-08 15:22:23 +0800

    Update inferenceservice-test.py

--- inferenceservice-test.py
+++ inferenceservice-test.py
@@ -65,11 +65,22 @@ with onepanel.core.api.ApiClient(configuration) as api_client:
 
 # In[8]:
 
-with open('./img.pkl','rb') as f:
-    img_data = pickle.load(f)
+# with open('./img.pkl','rb') as f:
+#     img_data = pickle.load(f)
+
+# data = {
+#     'instances': img_data
+# }
+import base64
+image = open('./persons.jpg', 'rb') #open binary file in read mode
+image_read = image.read()
+image_64_encode = base64.b64encode(image_read)
+bytes_array = image_64_encode.decode('utf-8')
 
 data = {
-    'instances': img_data
+    'instances': [
+        {'data': bytes_array}
+    ]
 }
 
 
